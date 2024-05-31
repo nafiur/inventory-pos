a808e8c23939de81576cd8ca9c469e9805116291
Nafiur Rahman <nrsajib@gmail.com>
2024-05-31 20:40:09 +0600

    Template Mastering # 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function Dashboard()
     {
+        $id = Auth::user()->id;
 
-        return view('backend.admin_master');
+        $user = User::find($id);
+
+        return view('backend.admin_master', compact('user'));
     }
 
 
